ccb2eeb2caa33b8db57fe6ac5ec1d6179f62ea36
orphybel <orphybel@gmail.com>
2025-10-23 16:26:11 +0200

    Updated VideosSection.tsx

--- src/components/VideosSection.tsx
+++ src/components/VideosSection.tsx
@@ -4,7 +4,7 @@ const VideosSection = () => {
 
   const videos = [
     {
-      title: 'Baila Morena',
+      title: 'L'OkaZ Millau 1 Août 2022 Proud Mary',
       thumbnail: 'https://img.youtube.com/vi/KBiUkjqczRY/maxresdefault.jpg',
       videoId: 'KBiUkjqczRY',
     },
