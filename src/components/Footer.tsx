6363e4429ef8ccb326bb2b5cb04e4ccdb0df5c9e
orphybel <orphybel@gmail.com>
2025-10-24 08:26:32 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="flex items-center mb-6 md:mb-0">
-            <img src="/logo-okaz-noir.avif" alt="L'OkaZ" className="h-10 brightness-0 invert" />
+            <img src="/logo-okaz copy.avif" alt="L'OkaZ" className="h-12" />
           </div>
 
           <div className="text-center md:text-right">
