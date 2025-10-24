05b75da59aa2c24f7b7f777b9de4afab9cbc6d44
orphybel <orphybel@gmail.com>
2025-10-24 16:50:40 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = ({ scrollToSection }: HeroProps) => {
         className="absolute inset-0 bg-center bg-no-repeat"
         style={{
           backgroundImage: 'url(/Affiche_groupe_foule.png)',
-          backgroundSize: '150%',
+          backgroundSize: '50%',
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-60"></div>
